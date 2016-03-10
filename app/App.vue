db80ced69d666197d02faafe812fc43d09a68458
xiaoluoboding <xiaoluoboding@gmail.com>
2016-03-10 09:28:48 +0800

    添加数据库支持

--- app/App.vue
+++ app/App.vue
@@ -2,10 +2,39 @@
     var Sidebar = require('./components/Sidebar.vue')
     var Login = require('./components/Login.vue')
 
+    // 连接mongodb
+    var MongoClient = require('mongodb').MongoClient,
+        assert = require('assert');
+
+    // Connection URL
+    var url = 'mongodb://8.1.3.213:27017/test';
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        findDocuments(db, function() {
+            console.log('callback');
+            db.close();
+        });
+    });
+
+    var findDocuments = function(db, callback) {
+        // Get the documents collection
+        var collection = db.collection('messagebox');
+        // Find some documents
+        collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            assert.equal(1, docs.length);
+            console.log("Found the following records");
+            console.dir(docs);
+            callback(docs);
+        });
+    }
+
     module.exports = {
         name: "App",
 
-        data: function () {
+        data: function() {
             return {
                 isLogin: false,
                 typeId: '',
@@ -29,7 +58,7 @@
     }
 </script>
 <template>
-    <div class="login-style"  v-if="!isLogin">
+    <div class="login-style" v-if="!isLogin">
         <login :msg.sync="isLogin"></login>
     </div>
     <div class="dashboard" v-if="isLogin">
